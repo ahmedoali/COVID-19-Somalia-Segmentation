f2cdf3cd1b423769eea6901e9a15d56a35a0f5df
Ahmed OM Ali <32178574+ahmedoali@users.noreply.github.com>
2021-11-08 19:41:23 +0000

    Create Data_cleaning.R

---
+++ Data_cleaning.R
@@ -0,0 +1,100 @@
+
+# Data cleaning -----------------------------------------------------------
+
+# Clear environment
+rm(list = ls())
+gc()
+
+# Read data in
+library(readr)
+COVID <- read_csv("~/SOM_2020_HFPS_v01_M_CSV/SHFPS_R1.csv")
+
+# Missing data check
+colSums(is.na(COVID))
+
+# Add unique id to data
+COVID$unique_id <- seq(1,nrow(COVID),1)
+COVID <- COVID[,c(563,1:562)]
+
+# Profiling data
+library(tidyverse)
+library(fastDummies)
+profiling_data <- COVID %>%
+  filter(itw_result == "complete") %>% 
+  dplyr::select(unique_id,CSW,age,state,region,ruralurban,idp_nomad,edu_level,govt_resp_sat,b_wash_hands,b_avoid_handshake,b_avoid_groups,covid_feel,covid_threat,
+                limit_rights,great_risk,money_misuse,flee_home,no_soap,edu_last_week,income_1:income_9,fies_no_food,
+                fies_hungry,fies_no_eat,cope_shock_1:cope_shock_9)
+
+profiling_data_dum <- dummy_cols(profiling_data[,-c(1:3,13:17)])
+profiling_data_dum <- profiling_data_dum[,-c(1:33)]
+profiling_data <- cbind(profiling_data[,c(1:3)],profiling_data_dum)
+
+profiling_data[is.na(profiling_data)] <- 0
+
+columns_to_remove <- grep("_NA",names(profiling_data))
+profiling_data <- profiling_data[,-columns_to_remove]
+
+columns_to_remove <- grep("_No",names(profiling_data))
+profiling_data <- profiling_data[,-columns_to_remove]
+
+profiling_data <- merge(profiling_data,COVID[,c(1,183:187)], by = "unique_id")
+
+
+# Input data
+input_data <- profiling_data[,c(1:3,52:55,79:83)]
+
+input_data <- input_data %>%
+  mutate(covid_feel_r = recode(covid_feel,`0` = 0,
+                               "Very worried" = 1,
+                               "Somewhat worried" = 2,
+                               "Not too worried" = 3,
+                               "Not worried at all" = 4),
+         covid_threat_r = recode(covid_threat,`0` = 0,
+                                 "A substantial threat" = 1,
+                                 "A moderate threat" = 2,
+                                 "Not much of a threat" = 3,
+                                 "Not a threat at all" = 4),
+         limit_rights_r = recode(limit_rights,`0` = 0,"Strongly disagree" = 1,
+                                 "Disagree" = 2,
+                                 "Neutral" = 3,
+                                 "Agree" = 4,
+                                 "Strongly agree" = 5),
+         great_risk_r = recode(great_risk,`0` = 0,"Strongly disagree" = 1,
+                               "Disagree" = 2,
+                               "Neutral" = 3,
+                               "Agree" = 4,
+                               "Strongly agree" = 5),
+         money_misuse_r = recode(money_misuse,`0` = 0,"Strongly disagree" = 1,
+                                 "Disagree" = 2,
+                                 "Neutral" = 3,
+                                 "Agree" = 4,
+                                 "Strongly agree" = 5))
+
+input_data <- input_data %>%
+  mutate(covid_feel_r_2tb = ifelse(covid_feel_r >= 3,1,0),
+         covid_threat_r_2tb = ifelse(covid_threat_r>=3,1,0),
+         limit_rights_r_2tb = ifelse(limit_rights_r>=4,1,0),
+         great_risk_r_2tb = ifelse(great_risk_r >= 4,1,0),
+         money_misuse_r_2tb = ifelse(money_misuse_r>=4,1,0),
+         covid_feel_r_2bb = ifelse(covid_feel_r == 1 | covid_feel_r == 2 ,1,0),
+         covid_threat_r_2bb = ifelse(covid_threat_r == 1 | covid_threat_r == 2,1,0),
+         limit_rights_r_2bb = ifelse(limit_rights_r == 1 | limit_rights_r == 2,1,0),
+         great_risk_r_2bb = ifelse(great_risk_r == 1 | great_risk_r == 2,1,0),
+         money_misuse_r_2bb = ifelse(money_misuse_r == 1 | money_misuse_r == 2,1,0))
+
+
+input_data <- input_data[,-c(8:12)]
+input_data <- input_data[,-c(8:12)]
+
+input_data[is.na(input_data)] <- 0
+
+# Input data - col std
+library(corpcor)
+input_data_colstd <- data.frame(input_data$unique_id,wt.scale(input_data[,-c(1:2)],input_data[,2]
+                              ,center = TRUE,scale = TRUE))
+
+# Randomise order
+set.seed(123)
+rows <- sample(nrow(input_data))
+input_data <- input_data[rows,]
+input_data_colstd <- input_data_colstd[rows,]
